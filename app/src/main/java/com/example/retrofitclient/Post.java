02bc029cd11b312e87c7ca2303185f84e29e4fe1
Tiam Abderezai <tiam.abderezai@gmail.com>
2020-04-16 02:12:10 -0700

    Initial commit

---
+++ app/src/main/java/com/example/retrofitclient/Post.java
@@ -0,0 +1,30 @@
+package com.example.retrofitclient;
+
+public class Post {
+
+    private int userId;
+    private int id;
+    private String title;
+
+//    @SerializedName("body")
+    private String text;
+
+    public Post() {
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
